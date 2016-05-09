a9862c7b03e439a2c678a61e7125443125e9b27c
kumagai <kumagai@toshiba-tops.co.jp>
2016-05-09 17:05:35 +0900

    add selection control for game center and game kind

--- script/gamelist.js
+++ script/gamelist.js
@@ -1,7 +1,102 @@
 
+var gameCenterList =
+	[
+		'綾瀬銀河',
+		'稲毛ヤングボウル',
+		'王子サンパレス',
+		'錦糸町NEWウイング',
+		'高円寺ゲームスタジオCUBE',
+		'自宅',
+		'十条モナコ',
+		'小平ピア２１',
+		'上野ダンディ',
+		'上野北欧',
+		'新橋ニュー新橋ビル',
+		'赤羽ブロックルーム',
+		'千葉ノーブル',
+		'船橋グランドサウナ',
+		'巣鴨サンフラワー',
+		'大久保キョンタゴン',
+		'大森みづほ',
+		'大塚ハイテクランドアカデミー',
+		'池袋プラザ',
+		'竹ノ塚アドベンチャー',
+		'天満オリンピア',
+		'東武練馬プレイハウス',
+		'梅ヶ丘アリア',
+		'柏柏泉',
+		'板橋ディンドン',
+		'北千住オモロン'
+	];
+
+var gameKindList =
+	[
+		'CAFE PARADISE',
+		'Double Bet麻雀',
+		'Return of SEL雀II',
+		'かぐや姫 其の二'
+		'かぐや姫',
+		'ジャンピューター96',
+		'ジャンピューターDX',
+		'ジャンピューターSP',
+		'雀神PLUS',
+		'麻雀 Cafe Break みこすり半',
+		'麻雀 SUPER大中華圏',
+		'麻雀 ゴージャスナイト',
+		'麻雀 スウィートアカデミー',
+		'麻雀 スウィートアカデミーSP',
+		'麻雀 宇宙の神秘',
+		'麻雀 黄金の牌',
+		'麻雀 沖縄娘',
+		'麻雀 海一発',
+		'麻雀 黒龍',
+		'麻雀 雀帝',
+		'麻雀 世界の神秘',
+		'麻雀 聖龍伝説',
+		'麻雀 大三元',
+		'麻雀 大車輪',
+		'麻雀 大中華圏',
+		'麻雀 大東洋圏',
+		'麻雀 大明神DELUXE',
+		'麻雀 大立直',
+		'麻雀 天神牌',
+		'麻雀 天鳳',
+		'麻雀 東洋の神秘',
+		'麻雀 東洋の神秘PART2',
+		'麻雀 雷神牌DX',
+		'麻雀 立直II',
+		'麻雀 立直一発',
+		'麻雀 連荘王'
+	];
+
 function initGameListPage()
 {
 	listGameListPage();
+
+	var i;
+	var gameCenterSelect = document.getElementById('gamecenter');
+
+	for (i=0 ; i<gameCenterList.length ; i++)
+	{
+	    var option = document.createElement('option');
+
+	    option.setAttribute('value', gameCenterList[i]);
+	    option.innerHTML = gameCenterList[i];
+
+	    gameCenterSelect.appendChild(option);
+	}
+
+	var gameKindSelect = document.getElementById('gamekind');
+
+	for (i=0 ; i<gameKindList.length ; i++)
+	{
+	    var option = document.createElement('option');
+
+	    option.setAttribute('value', gameKindList[i]);
+	    option.innerHTML = gameKindList[i];
+
+	    gameKindSelect.appendChild(option);
+	}
 }
 
 function addGame()
